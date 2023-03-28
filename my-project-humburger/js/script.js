f4342076a1ef82f1dc7037e08565652e87de1b16
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-03-28 19:55:36 +0300

    homework

--- my-project-humburger/js/script.js
+++ my-project-humburger/js/script.js
@@ -2,19 +2,27 @@ $(document).ready(function () {
   $('#scr').on('click', function () {
     if ($(this).attr('src') === '../icons/hamburger-menu-462145.webp') {
       $(this).attr('src', 'https://www.svgrepo.com/show/12848/x-symbol.svg');
-      $('#container-check-box').toggleClass('active');
+      $('#container-check-box').addClass('openAnimateDiv').removeClass('closeAnimateDiv').css('display', 'block').off('animationend').on('animationend', function () {
+        $('#container-check-box').removeClass('openAnimateDiv');
+      });
     } else {
       $(this).attr('src', '../icons/hamburger-menu-462145.webp');
-      $('#container-check-box').toggleClass('active');
+      $('#container-check-box').addClass('closeAnimateDiv').removeClass('openAnimateDiv').off('animationend').on('animationend', function () {
+        $('#container-check-box').removeClass('closeAnimateDiv').css('display', 'none');
+      });
     }
   });
 
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    lazyLoad: true,
-    lazyLoadEager: 1,
-    loop: true,
-    margin: 10,
-    autoHeight: true
-  });
+$('.custom1').owlCarousel({
+  animateOut: 'slideOutDown',
+  animateIn: 'flipInX',
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 13000,
+  autoplayHoverPause: true,
+  items: 1,
+  margin: 30,
+  stagePadding: 30,
+  smartSpeed: 450
+});
 });
